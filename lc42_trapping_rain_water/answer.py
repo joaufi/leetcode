fad90233bd22a5bf4ac3e815e55108e9389f05d6
Johnathan Fisher <joaufi@giv.local>
2020-03-01 15:50:12 -0500

    Solution with better memory constraints

--- lc42_trapping_rain_water/answer.py
+++ lc42_trapping_rain_water/answer.py
@@ -1,32 +1,54 @@
 from typing import List
 
 
-def get_water_trapped(elevation_coordinates: List[int]):
-    # turn elevation coordinates into a matrix
-    # example: [0, 2, 0, 1]
-    # becomes: [[1, 0, 1, 0],
-    #           [1, 0, 1, 1]]
-    # 0 is a wall space and 1 is an "empty" space
-    y_max = max(elevation_coordinates)
-    matrix = [
-        [
-            0 if y < height else 1
-            for height in elevation_coordinates
-        ]
-        for y in range(y_max)
-    ]
+# def get_water_trapped(elevation_coordinates: List[int]):
+#     # turn elevation coordinates into a matrix
+#     # example: [0, 2, 0, 1]
+#     # becomes: [[1, 0, 1, 0],
+#     #           [1, 0, 1, 1]]
+#     # 0 is a wall space and 1 is an "empty" space
+#     y_max = max(elevation_coordinates)
+#     matrix = [
+#         [
+#             0 if y < height else 1
+#             for height in elevation_coordinates
+#         ]
+#         for y in range(y_max)
+#     ]
+#
+#     # find the index of the first wall position and the index of the last wall
+#     # position, then sum all of the "empty" spaces between those two walls.
+#     units = 0
+#     for row in matrix:
+#         first_wall = row.index(0)
+#         # list[::-1] reverses the list; if the last wall position is at index 0
+#         # it is really at the -1 th space in the list, thus the need for the or
+#         last_wall = row[::-1].index(0) or 1
+#         units += sum(row[first_wall:-last_wall])
+#
+#     return units
 
-    # find the index of the first wall position and the index of the last wall
-    # position, then sum all of the "empty" spaces between those two walls.
+def trap(heights: List[int]) -> int:
+    if not heights:
+        return 0
+
     units = 0
-    for row in matrix:
+    max_height = max(heights)
+    min_height = min(heights)
+    for current_height in range(min_height, max_height):
+        row = []
+        last_wall = 0
+        for i, height in enumerate(heights):
+            is_empty_space = 0 if current_height < height else 1
+            if not is_empty_space:
+                last_wall = i
+            row.append(is_empty_space)
         first_wall = row.index(0)
-        # list[::-1] reverses the list; if the last wall position is at index 0
-        # it is really at the -1 th space in the list, thus the need for the or
-        last_wall = row[::-1].index(0) or 1
-        units += sum(row[first_wall:-last_wall])
+        units += sum(row[first_wall:last_wall])
 
     return units
+
+
 
 
 
--- lc42_trapping_rain_water/tests.py
+++ lc42_trapping_rain_water/tests.py
@@ -1,9 +1,9 @@
-from lc42_trapping_rain_water.answer import get_water_trapped
+from lc42_trapping_rain_water.answer import trap
 
 def test_get_water_trapped__valid_elevation_coordinates__return_units_trapped_water():
     elevation_coordinates = [0,1,0,2,1,0,1,3,2,1,2,1]
     expected = 6
 
-    actual = get_water_trapped(elevation_coordinates)
+    actual = trap(elevation_coordinates)
 
     assert expected == actual
